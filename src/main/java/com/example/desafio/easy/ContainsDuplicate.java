e3e60354741e0f08811a0da754e855e595040556
Jairo Pinheiro <jairopnhr02@gmail.com>
2025-07-31 13:58:42 -0300

    feat: Contains Duplicate

--- src/main/java/com/example/desafio/easy/ContainsDuplicate.java
+++ src/main/java/com/example/desafio/easy/ContainsDuplicate.java
@@ -1,4 +1,22 @@
 package com.example.desafio.easy;
 
 public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        int size = nums.length;
+        for (int i = 0; i < size - 1 ; i++) {
+            for (int j = i + 1; j  < size ; j++) {
+                System.out.println(nums[i] + " ++++ "+  nums[j]);
+                if (nums[j] == nums[i]){
+                    //System.out.println(nums[i] + " ++++ "+  nums[j]);
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1,2,3,4,5,5}));
+    }
 }
